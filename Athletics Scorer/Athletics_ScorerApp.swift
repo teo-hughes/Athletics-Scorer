50750a3f7edb625225502fa4a69a64c6d034efd1
Teo <hughes.t@etoncollege.org.uk>
2022-05-19 11:05:22 +0100

    Initial Commit

---
+++ Athletics Scorer/Athletics_ScorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Athletics_ScorerApp.swift
+//  Athletics Scorer
+//
+//  Created by Hughes, Teo (BJH) on 19/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct Athletics_ScorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
